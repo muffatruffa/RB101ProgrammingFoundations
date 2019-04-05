f8e1a570033313cb064dcb285e760c0c293e6570
pippo <schembrigiuseppe@yahoo.it>
2019-04-05 12:00:13 +0200

    Added twenty one

--- lesson_6/test_twenty_one.rb
+++ lesson_6/test_twenty_one.rb
@@ -11,5 +11,15 @@ class TwentyOneTest < Minitest::Test
     assert_equal(@deck.first, [:hearts, 1])
     assert_equal(@deck.last, [:spades, 13])
   end
+
+  def test_cards_values
+    crds = [1]
+    assert_equal([11], cards_values(crds))
+    crds = [1, 8, 1]
+    assert_equal([11, 8, 1], cards_values(crds))
+    crds = [1, 4, 1, 5]
+    assert_equal([11, 4, 1, 5], cards_values(crds))
+
+  end
 end
 
